c21030453d74826bfd6db9966927c84da7022f56
Maret <maret.stoffberg@gmail.com>
2024-08-12 23:28:24 +0300

    Update OrderByTableView to have accessoryType when selected

--- About-You/Main Files (Free to change)/Controllers/OrderByTableViewController.swift
+++ About-You/Main Files (Free to change)/Controllers/OrderByTableViewController.swift
@@ -23,14 +23,19 @@ class OrderByTableViewController: UITableViewController {
             return cell ?? UITableViewCell()
         }
         cell.textLabel?.text = orderOptions[indexPath.row].rawValue
+        cell.accessoryType = .none
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
         guard let delegate, indexPath.row < orderOptions.count else {
             return
         }
+        
+        for cell in tableView.visibleCells {
+            cell.accessoryType = .none
+        }
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         delegate.didSelectOrder(orderOptions[indexPath.row])
     }
 
