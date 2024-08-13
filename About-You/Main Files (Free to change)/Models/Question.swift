4e15ee3f02611fe447f502a0cd45ae2d13808583
Maret <maret.stoffberg@gmail.com>
2024-08-13 22:11:54 +0300

    Update alignment in Questions.swift

--- About-You/Main Files (Free to change)/Models/Question.swift
+++ About-You/Main Files (Free to change)/Models/Question.swift
@@ -26,55 +26,73 @@ struct Answer: Codable {
 
 class MainQuestions {
     static func questionOne(answer: Answer) -> Question {
-        return Question(questionText: "When do you have the most energy?",
-                        answerOptions: ["6am", "12pm", "6pm", "Midnight"],
-                        questionType: .selection,
-                        answer: answer)
+        return Question(
+            questionText: "When do you have the most energy?",
+            answerOptions: ["6am", "12pm", "6pm", "Midnight"],
+            questionType: .selection,
+            answer: answer
+        )
     }
 
     static func questionTwo(answer: Answer) -> Question {
-        return Question(questionText: "How old were you when you wrote your first line of code?",
-                        answerOptions: ["Younger than 10 years",
-                                        "10 to 15 years old",
-                                        "16 to 20 years old",
-                                        "21 to 25 years old",
-                                        "26 years old or older"],
-                        questionType: .selection,
-                        answer: answer)
+        return Question(
+            questionText: "How old were you when you wrote your first line of code?",
+            answerOptions: [
+                "Younger than 10 years",
+                "10 to 15 years old",
+                "16 to 20 years old",
+                "21 to 25 years old",
+                "26 years old or older"
+            ],
+            questionType: .selection,
+            answer: answer
+        )
     }
 
     static func questionThree(answer: Answer) -> Question {
-        return Question(questionText: "Which new programming language would you want to learn?",
-                        answerOptions: ["Python",
-                                        "Kotlin",
-                                        "Swift",
-                                        "Ruby",
-                                        "C#",
-                                        "C++",
-                                        "Rust",
-                                        "None"],
-                        questionType: .selection,
-                        answer: answer)
+        return Question(
+            questionText: "Which new programming language would you want to learn?",
+            answerOptions: [
+                "Python",
+                "Kotlin",
+                "Swift",
+                "Ruby",
+                "C#",
+                "C++",
+                "Rust",
+                "None"
+            ],
+            questionType: .selection,
+            answer: answer
+        )
     }
 
     static func questionFour(answer: Answer) -> Question {
-        return Question(questionText: "How often do you learn a new framework or language?",
-                        answerOptions: ["Every few months",
-                                        "Once a year",
-                                        "Once every few years"],
-                        questionType: .selection,
-                        answer: answer)
+        return Question(
+            questionText: "How often do you learn a new framework or language?",
+            answerOptions: [
+                "Every few months",
+                "Once a year",
+                "Once every few years"
+            ],
+            questionType: .selection,
+            answer: answer
+        )
     }
 
     static func questionFive(answer: Answer) -> Question {
-        return Question(questionText: "What do you do when you get stuck on a problem?",
-                        answerOptions: ["Visit Stack Overflow",
-                                        "Do other work and come back later",
-                                        "Call a coworker or friend",
-                                        "Watch or read a tutorial",
-                                        "Go down a google rabbit hole",
-                                        "Panic"],
-                        questionType: .selection,
-                        answer: answer)
+        return Question(
+            questionText: "What do you do when you get stuck on a problem?",
+            answerOptions: [
+                "Visit Stack Overflow",
+                "Do other work and come back later",
+                "Call a coworker or friend",
+                "Watch or read a tutorial",
+                "Go down a google rabbit hole",
+                "Panic"
+            ],
+            questionType: .selection,
+            answer: answer
+        )
     }
 }
